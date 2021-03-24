790808bd2ae3169c87639a60670db0b28c777300
ymartinguzman <yamiramartinguzman@gmail.com>
2021-03-24 21:03:17 +0100

    add events to project

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const containerText = document.querySelector('.js-containerText');
+const description = document.querySelector('.js-text');
+
+function showText() {
+  description.classList.remove('js-text');
+  description.classList.add('js-projects');
+}
+containerText.addEventListener('click', showText);

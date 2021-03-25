4f5b5fd3f63dd42e3da9007e6489e377a442b4cf
ymartinguzman <yamiramartinguzman@gmail.com>
2021-03-25 19:12:31 +0100

    add event to info of each project

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,14 @@
 
 const containerText = document.querySelector('.js-containerText');
 const description = document.querySelector('.js-text');
+const id = document.querySelector('#id');
 
 function showText() {
-  description.classList.remove('js-text');
-  description.classList.add('js-projects');
+  if (description.id !== 0) {
+    description.classList.remove('js-text');
+    description.classList.add('js-projects');
+  }
 }
 containerText.addEventListener('click', showText);
+console.log(containerText);
+console.log(description.id);

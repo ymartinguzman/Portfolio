24a273680b1ed05cf0317b1b3df7cc61dfc5b723
ymartinguzman <yamiramartinguzman@gmail.com>
2021-03-26 12:04:12 +0100

    add mouseover event to projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const containerText = document.querySelector('.js-containerText');
-const description = document.querySelector('.js-text');
-const id = document.querySelector('#id');
+const containerText = document.querySelectorAll('.js-containerText');
+const description = document.querySelectorAll('.js-text');
 
-function showText() {
-  if (description.id !== 0) {
-    description.classList.remove('js-text');
-    description.classList.add('js-projects');
-  }
+function showText(event) {
+  const id = parseInt(event.currentTarget.id);
+  console.log(id);
+
+  description[id].classList.remove('js-text');
+  description[id].classList.add('js-projects');
 }
-containerText.addEventListener('click', showText);
-console.log(containerText);
-console.log(description.id);
+function hideText(event) {
+  const id = event.currentTarget.id;
+  description[id].classList.add('js-text');
+  description[id].classList.remove('js-projects');
+}
+
+for (let i = 0; i < containerText.length; i++) {
+  containerText[i].addEventListener('mouseover', showText);
+  containerText[i].addEventListener('mouseout', hideText);
+}

feb896fcb713fcd44e48a2e8415c1361fe16f4ec
ymartinguzman <yamiramartinguzman@gmail.com>
2021-04-07 18:14:01 +0200

    add js to technologies

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,12 @@
 
 const containerText = document.querySelectorAll('.js-containerText');
 const description = document.querySelectorAll('.js-text');
+const techName = document.querySelectorAll('.js-title');
+const techContainer = document.querySelectorAll('.js-techContainer');
 
+//Projects
 function showText(event) {
   const id = parseInt(event.currentTarget.id);
-  console.log(id);
-
   description[id].classList.remove('js-text');
   description[id].classList.add('js-projects');
 }
@@ -20,3 +21,23 @@ for (let i = 0; i < containerText.length; i++) {
   containerText[i].addEventListener('mouseover', showText);
   containerText[i].addEventListener('mouseout', hideText);
 }
+
+//Technologies
+function showTech(event) {
+  const id = parseInt(event.currentTarget.id);
+  console.log(id);
+  techName[id].classList.remove('js-title');
+  techName[id].classList.add('js-tech');
+}
+function hideTech(event) {
+  const id = event.currentTarget.id;
+
+  techName[id].classList.add('js-title');
+  techName[id].classList.remove('js-tech');
+}
+for (let i = 0; i < techContainer.length; i++) {
+  techContainer[i].addEventListener('mouseover', showTech);
+  techContainer[i].addEventListener('mouseout', hideTech);
+}
+console.log(techName.id);
+console.log(techContainer);

c5aff80a247b6778be4e16adc7668b97e6784750
ymartinguzman <yamiramartinguzman@gmail.com>
2021-04-26 17:33:55 +0200

    add english button to translate

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,40 @@
 'use strict';
 
 const containerText = document.querySelectorAll('.js-containerText');
+const containerEnglish = document.querySelectorAll('.js-containerEnglish');
 const description = document.querySelectorAll('.js-text');
+const englishText = document.querySelectorAll('.js-text-en');
 const techName = document.querySelectorAll('.js-title');
 const techContainer = document.querySelectorAll('.js-techContainer');
+const spanishBtn = document.querySelector('.js__btn-es');
+const englishBtn = document.querySelector('.js__btn-en');
+const english = document.querySelectorAll('.js-english');
+const spanish = document.querySelectorAll('.js-spanish');
 
 //Projects
 function showText(event) {
   const id = parseInt(event.currentTarget.id);
-  description[id].classList.toggle('js-projects');
-  description[id].classList.toggle('js-text');
+  description[id].classList.add('js-projects');
+  description[id].classList.remove('js-text');
+  englishText[id].classList.add('js-projects');
+  englishText[id].classList.remove('js-text-en');
 }
 function hideText(event) {
   const id = event.currentTarget.id;
-  description[id].classList.toggle('js-text');
-  description[id].classList.toggle('js-projects');
+  description[id].classList.add('js-text');
+  description[id].classList.remove('js-projects');
+  englishText[id].classList.add('js-text-en');
+  englishText[id].classList.remove('js-projects');
 }
 
 for (let i = 0; i < containerText.length; i++) {
   containerText[i].addEventListener('mouseover', showText);
   containerText[i].addEventListener('mouseout', hideText);
+}
+
+for (let i = 0; i < containerEnglish.length; i++) {
+  containerEnglish[i].addEventListener('mouseover', showText);
+  containerEnglish[i].addEventListener('mouseout', hideText);
 }
 
 //Technologies
@@ -38,3 +53,23 @@ for (let i = 0; i < techContainer.length; i++) {
   techContainer[i].addEventListener('mouseover', showTech);
   techContainer[i].addEventListener('mouseout', hideTech);
 }
+
+//Change language
+function changeToEnglish() {
+  for (let i = 0; i < english.length; i++) {
+    english[i].classList.remove('hidden-text');
+    spanish[i].classList.add('hidden-text');
+  }
+
+  console.log(english.length);
+  console.log(spanish.length);
+}
+function changeToSpanish() {
+  for (let i = 0; i < spanish.length; i++) {
+    spanish[i].classList.remove('hidden-text');
+    english[i].classList.add('hidden-text');
+  }
+}
+
+englishBtn.addEventListener('click', changeToEnglish);
+spanishBtn.addEventListener('click', changeToSpanish);
